51c3c3bc6c8a1afdeb6c259da22c8b8903ea812c
Mayuu Furuki <aaa08027744484@gmail.com>
2020-11-24 19:16:41 +0900

    Make a basic User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ApplicationRecord
+before_save {email.downcase!}
   with_options presence: true do
     validates :name, length: {maximum: 50}
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-    validates :email, length: {maximum: 255}, format:{with: VALID_EMAIL_REGEX }
+    validates :email, length: {maximum: 255},
+               format:{with: VALID_EMAIL_REGEX },
+               uniqueness: true
+    validates :password, length: {minimum: 6}
   end
+  has_secure_password
 end
---
+++ db/migrate/20201124091849_add_index_to_usres_email.rb
@@ -0,0 +1,5 @@
+class AddIndexToUsresEmail < ActiveRecord::Migration[6.0]
+  def change
+    add_index :users, :email, unique: true
+  end
+end
