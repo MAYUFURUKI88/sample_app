222b61f872b548bf115a7f860fc15f059f81df18
Mayuu Furuki <aaa08027744484@gmail.com>
2020-11-24 17:52:03 +0900

    email validation

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  with_options presence: true do
+    validates :name, length: {maximum: 50}
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    validates :email, length: {maximum: 255}, format:{with: VALID_EMAIL_REGEX }
+  end
+end
