d120c96ee5892798d20536be99f7ef47ef24d909
Mayuu Furuki <aaa08027744484@gmail.com>
2020-12-10 17:34:56 +0900

    Implement basic login

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,10 @@ before_save {email.downcase!}
     validates :password, length: {minimum: 6}
   end
   has_secure_password
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                 BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
