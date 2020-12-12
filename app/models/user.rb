9c7ccc3ccd04b60b46feed5df655de7a14785b72
Mayuu Furuki <aaa08027744484@gmail.com>
2020-12-12 18:06:45 +0900

    form

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,9 @@ before_save {email.downcase!}
     validates :email, length: {maximum: 255},
                format:{with: VALID_EMAIL_REGEX },
                uniqueness: true
-    validates :password, length: {minimum: 6}
   end
   has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
 class << self
   def digest(string)
